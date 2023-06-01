a8440c1d97e0f057c1639e518bd09720df8d674a
shivamvishwakarma365 <78745548+shivamvishwakarma365@users.noreply.github.com>
2023-06-01 23:31:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// your code here
-function updateURL() {
+ function updateURL() {
       var name = document.getElementById("name").value;
       var year = document.getElementById("year").value;
       var url = "https://localhost:8080/?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
